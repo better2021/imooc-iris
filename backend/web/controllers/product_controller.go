0058c4d7ca97facd86a5aa2c11a042dd6c837339
feiyu <feiyu@tg10010.com>
2019-08-21 18:40:15 +0700

    'ss'

---
+++ backend/web/controllers/product_controller.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"imooc-iris/common"
+	"imooc-iris/datamodels"
+	"imooc-iris/services"
+)
+
+type ProductController struct {
+	Ctx iris.Context
+	ProductService services.IProductService
+}
+
+func (p *ProductController) GetAll() mvc.View{
+	productArray,_ := p.ProductService.GetAllProduct()
+	return mvc.View{
+		Name:"product/view.html",
+		Data:iris.Map{
+			"productArray":productArray,
+		},
+	}
+}
+
+// 修改商品
+func (p *ProductController) PostUpdate() {
+	product := &datamodels.Product{}
+	p.Ctx.Request().ParseForm()
+	dec := common.NewDecoder(&common.DecoderOptions{TagName:"imooc"})
+	if err := dec.Decode(p.Ctx.Request().Form,product);err !=nil{
+		p.Ctx.Application().Logger().Debug()
+	}
+	err:= p.ProductService.UpdateProduct(product)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug()
+	}
+	p.Ctx.Redirect("/product/all")
+}
--- backend/web/main.go
+++ backend/web/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"imooc-iris/backend/web/controllers"
+	"imooc-iris/common"
+	"imooc-iris/repositories"
+	"imooc-iris/services"
 )
 
 func main(){
@@ -20,7 +27,20 @@ func main(){
 		ctx.ViewLayout("")
 		ctx.View("shared/error.html")
 	})
+	// 连接数据库
+	db,err := common.NewMysqlConn()
+	if err !=nil{
+		fmt.Println(err)
+	}
+	ctx,cancel := context.WithCancel(context.Background())
+	defer cancel()
 	// 5.注册控制器
+	productRepository := repositories.NewProductManager("product",db)
+	productService := services.NewProductService(productRepository)
+	productParty := app.Party("/product")
+	product := mvc.New(productParty)
+	product.Register(ctx,productService)
+	product.Handle(new(controllers.ProductController))
 
 	// 6.启动服务
 	app.Run(
